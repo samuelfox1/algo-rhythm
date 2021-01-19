3c21fca6ec52e881e45282e254702f4850592ecf
Samuel Fox <samueljasonfox@gmail.com>
2021-01-19 01:58:49 -0800

    solved week 6 challenges

---
+++ 06-Server-Side-APIs/03-title-case/SOLVED/title-case.js
@@ -0,0 +1,12 @@
+// Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
+
+var titleCase = function (str) {
+    let sentence = []
+    let x = str.split(' ').forEach(word => {
+        let letters = word.split('')
+        letters[0] = letters[0].toUpperCase()
+        word = letters.join('')
+        sentence.push(word)
+    });
+    return sentence.join(' ')
+};
