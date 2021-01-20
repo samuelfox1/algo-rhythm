98c67f48008c341dc8404880b39ed364cacb5968
Samuel Fox <samueljasonfox@gmail.com>
2021-01-19 21:03:10 -0800

    solved longest-word

---
+++ 07-Project-1/02-longest-word/SOLVED/longest-word.js
@@ -0,0 +1,17 @@
+// Write code to create a function that accepts a string (`str`) and returns the longest word in the string
+
+var longestWord = function (str) {
+    let strArr = str.split(' ')
+    let longestCount = 0
+    let saveWord
+    strArr.forEach(word => {
+        let x = word.split('')
+        for (let i = 0; i < x.length; i++) {
+            if (longestCount < i) {
+                saveWord = word
+                longestCount = i
+            }
+        }
+    });
+    return saveWord
+}
