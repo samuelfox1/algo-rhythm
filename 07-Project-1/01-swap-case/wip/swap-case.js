6afcefe5635a18a3c2d9dc567a77eaaf22420dbc
Samuel Fox <samueljasonfox@gmail.com>
2021-01-19 20:38:41 -0800

    move unsolved copy to 'unsolved' branch, keep solved algos on main branch

--- 07-Project-1/01-swap-case/wip/swap-case.js
+++ 07-Project-1/01-swap-case/wip/swap-case.js
@@ -1,3 +1,12 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
-var swapCase = function(str) {};
+var swapCase = function (str) {
+    let x = str.toLowerCase().split('')
+    let y = str.split('')
+    var z = []
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === y[i]) { z.push(x[i].toUpperCase()) }
+        else { z.push(x[i]) }
+    }
+    return z.join('')
+};
