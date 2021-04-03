4ab62e6897f84a2e6ac5b71aa6482112ed0381f1
freeStoke <samueljasonfox@gmail.com>
2021-04-03 09:42:49 -0700

    jest setup and working for pairs-of-socks algorythm

--- pairs-of-socks/pairs-of-socks.js
+++ pairs-of-socks/pairs-of-socks.js
@@ -1,56 +1,41 @@
-'use strict';
 
-const n = 15
-// const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
-const ar = [6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5]
+// const n = 9
+// const arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
-// function sockMerchant(n, ar) {
-//     let numberOfmatches = 0
+// const n = 15
+// const arr = [6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5]
 
-//     for (let i = 0; i < ar.length; i++) {
-//         if (i === ar.length - 1) return numberOfmatches
-//         let searching = true
-//         let sock1 = ar[i]
+// const n = 20
+// const arr = [4, 5, 5, 5, 6, 6, 4, 1, 4, 4, 3, 6, 6, 3, 6, 1, 4, 5, 5, 5]
 
-//         for (let j = i + 1; j < ar.length; j++) {
-//             let sock2 = ar[j]
+function pairsOfSocks(arr) {
+    if (typeof arr === 'string' ||
+        typeof arr === 'number' ||
+        arr.length < 2) return null // if arr is a string, number, or not long enough
 
-//             if (searching && sock1 === sock2) {
-//                 numberOfmatches++
-//                 ar.splice(j, 1)
-//                 searching = false
-//                 console.log(sock1, sock2)
-//                 console.log(ar)
-//             }
-//         }
-//     }
-// }
-// Complete the sockMerchant function below.
-function sockMerchant(n, ar) {
-    let numberOfmatches = 0
-    console.log(ar)
+    let sockPile = arr // copy of data source
+    let numberOfmatches = 0 //counter for matches
+    // console.log(arr)
 
-    for (let i = 0; i < ar.length; i++) {
-        if (i + 1 === n) return numberOfmatches
-        let sock1 = ar[i]
-        let searching = true
+    for (let i = 0; i < sockPile.length; i++) { // select sock1
+        if (i + 1 === sockPile.length) return numberOfmatches // if there is not a sock2
+        let sock1 = sockPile[i] // easier to read
 
-        for (let j = i + 1; j < ar.length; j++) {
-            let sock2 = ar[j]
+        for (let j = i + 1; j < sockPile.length; j++) { // search for a match to sock 1
+            let sock2 = sockPile[j] // easier to read
 
-            if (searching && sock1 === sock2) {
-                numberOfmatches++
-                n--
-                console.log(sock1, sock2)
-                ar.splice(j, 1) // remove sock 2 from array
-                console.log(ar)
-                searching = false
+            if (sock1 === sock2) {  // if a match is found
+                numberOfmatches++ // increment counter
+                // console.log(sock1, sock2)
+                sockPile.splice(j, 1) // remove sock 2 from array FIRST to remove right to left
+                sockPile.splice(i, 1) // remove sock 1 from array
+                i-- // roll i back to componsate for mutation
+                // console.log(sockPile)
+                if (i + 1 === sockPile.length) return numberOfmatches // check number of socks remaining
+                break // exit loop
             }
         }
-        console.log(i, '==========')
     }
 }
 
-
-
-console.log(sockMerchant(n, ar))
+module.exports = pairsOfSocks
---
+++ pairs-of-socks/pairs-of-socks.test.js
@@ -0,0 +1,31 @@
+const { test, expect, toBe, it } = require('@jest/globals')
+const pairsOfSocks = require('./pairs-of-socks')
+
+describe('pairsOfSocks', () => {
+    it("should return 3 when given the array [10, 20, 20, 10, 10, 30, 50, 10, 20]", () => {
+        const arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+        const result = pairsOfSocks(arr);
+        expect(result).toBe(3);
+    });
+    it("should return 4 when given the array [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]", () => {
+        const arr = [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]
+        const result = pairsOfSocks(arr);
+        expect(result).toBe(4);
+    });
+    it("should return 1 when given the array [1,1]", () => {
+        const arr = [1, 1]
+        const result = pairsOfSocks(arr)
+        expect(result).toBe(1)
+    })
+    it("should return null when given array [1]", () => {
+        const arr = [1]
+        const result = pairsOfSocks(arr)
+        expect(result).toBe(null)
+    })
+    it("should return null when given a string 'testing'", () => {
+        const string = 'testing'
+        const result = pairsOfSocks(string)
+        expect(result).toBe(null)
+    })
+
+})
