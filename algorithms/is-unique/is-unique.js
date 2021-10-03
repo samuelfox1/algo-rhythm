ddbf9147d27b1ca57f67e9082c4e45769c8434b6
Samuel Fox <samueljasonfox@gmail.com>
2021-10-03 13:16:46 -0700

    update: move hr-new-years-chaos into algorithms folder

--- algorithms/is-unique/is-unique.js
+++ algorithms/is-unique/is-unique.js
@@ -1,15 +1,14 @@
 // Write code to create a function that accepts an array of numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-const isUnique = (arr) => {
-    let x = {}
-    for (let i = 0; i < arr.length; i++) {
-        let y = arr[i]
-        // if y already exists in object x, return false
-        if (x[y]) { return false }
-        // add key of y with value of true to object x
-        x[y] = true
+module.exports = {
+    isUnique: (arr) => {
+        let obj = {}
+        for (let i = 0; i < arr.length; i++) {
+            let key = arr[i]
+            if (obj[key]) return false  // if key already exists, return false
+            obj[key] = 1 // add key to obj with value of 1 to obj
+        }
+        return true
     }
-    return true
-};
-module.exports = isUnique
+}
